dd402322525543ace9d5d978b5c29b5b302d577f
akku750156 <akashtiwari750156@gmail.com>
2022-05-22 21:18:35 +0530

    Initial Commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Chats</title>
+        <meta name="description" />
+        <link rel="icon" href="/icon" />
+      </Head>
+    </div>
+  );
+}
