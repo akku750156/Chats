f48cfba320d348a86a71ad1183e5c473b62fd939
akku750156 <akashtiwari750156@gmail.com>
2022-05-23 18:25:07 +0530

    Navba done ✅

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import Head from "next/head";
 import Image from "next/image";
+import { useTheme } from "next-themes";
+import Navbar from "../components/Navbar";
 export default function Home() {
+  const { theme, setTheme } = useTheme();
   return (
     <div>
       <Head>
@@ -8,6 +11,16 @@ export default function Home() {
         <meta name="description" />
         <link rel="icon" href="/icon.png" />
       </Head>
+      <div className="h-screen m-6 p-6 rounded-[40px] shadow-[0px_0px_150px_5px_rgba(75,0,130,1)] ">
+        <Navbar />
+        <h1>Hello,World !</h1>
+        <button
+          className="text-blue-500 bg-blue-300 p-1 px-4 rounded-xl"
+          onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+        >
+          Sun
+        </button>
+      </div>
     </div>
   );
 }
